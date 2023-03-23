f7036191ae68b3e3c006c5fa8b2b27bb4049d296
Robbe Bierebeeck <robbebierebeeck@laptop-6vapcj9a.guests.thomasmore.be>
2023-03-23 16:17:05 +0100

    style: formatting

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,29 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
+    plugins: ['react', '@typescript-eslint'],
+    rules: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    }
 }
