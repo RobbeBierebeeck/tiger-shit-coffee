9d99d1837e0155012d1e76ab97aac0dc9791cbf8
Robbe Bierebeeck <robbebierebeeck@Robbes-MacBook-Pro-6.local>
2023-05-29 12:51:13 +0200

    fix: imports

--- server/controllers/api/v1/transactions.js
+++ server/controllers/api/v1/transactions.js
@@ -1,4 +1,4 @@
-const Transaction = require("./../../../models/Transaction");
+const Transaction = require("./../../../models/transaction");
 const User = require("./../../../models/user");
 
 const createTransaction = async (req, res) => {
